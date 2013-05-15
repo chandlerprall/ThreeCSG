7087213d98b47fd604ccc85172fb606a24e80e9d
Yaroslav Shirokov <slava@github.com>
2013-05-15 15:36:41 -0700

    Functional Vertex replacement

--- src/ThreeCSG.coffee
+++ src/ThreeCSG.coffee
@@ -1,3 +1,20 @@
-class window.ThreeBSP extends js_ThreeBSP
-  constructor: (args...) ->
-    super args...
+V3 = new THREE.Vector3()
+V2 = new THREE.Vector2()
+
+class ThreeBSP.Vertex extends THREE.Vector3
+  constructor: (x, y, z, @normal=V3.clone(), @uv=V2.clone()) ->
+    super x, y, z
+
+    # Method aliases
+    @subtract = @sub
+
+  clone: ->
+    new ThreeBSP.Vertex @x, @y, @z, @normal.clone(), @uv.clone()
+
+  lerp: (v, alpha) =>
+    @normal.add (v.normal ? V3).clone().sub(@normal).multiplyScalar(alpha)
+    @uv.add (v.uv ? V2).clone().sub(@uv).multiplyScalar(alpha)
+    super v, alpha
+
+  interpolate: (args...) =>
+    @clone().lerp args...
--- src/ThreeCSG.js
+++ src/ThreeCSG.js
@@ -1,5 +1,5 @@
 'use strict';
-window.js_ThreeBSP = (function() {
+window.ThreeBSP = (function() {
 
 	var ThreeBSP,
 		EPSILON = 1e-5,
@@ -338,90 +338,7 @@ window.js_ThreeBSP = (function() {
 		}
 	};
 
-	ThreeBSP.Vertex = function( x, y, z, normal, uv ) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.normal = normal || new THREE.Vector3;
-		this.uv = uv || new THREE.Vector2;
-	};
-	ThreeBSP.Vertex.prototype.clone = function() {
-		return new ThreeBSP.Vertex( this.x, this.y, this.z, this.normal.clone(), this.uv.clone() );
-	};
-	ThreeBSP.Vertex.prototype.add = function( vertex ) {
-		this.x += vertex.x;
-		this.y += vertex.y;
-		this.z += vertex.z;
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.subtract = function( vertex ) {
-		this.x -= vertex.x;
-		this.y -= vertex.y;
-		this.z -= vertex.z;
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.multiplyScalar = function( scalar ) {
-		this.x *= scalar;
-		this.y *= scalar;
-		this.z *= scalar;
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.cross = function( vertex ) {
-		var x = this.x,
-			y = this.y,
-			z = this.z;
-
-		this.x = y * vertex.z - z * vertex.y;
-		this.y = z * vertex.x - x * vertex.z;
-		this.z = x * vertex.y - y * vertex.x;
-
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.normalize = function() {
-		var length = Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
-
-		this.x /= length;
-		this.y /= length;
-		this.z /= length;
-
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.dot = function( vertex ) {
-		return this.x * vertex.x + this.y * vertex.y + this.z * vertex.z;
-	};
-	ThreeBSP.Vertex.prototype.lerp = function( a, t ) {
-		this.add(
-			a.clone().subtract( this ).multiplyScalar( t )
-		);
-
-		this.normal.add(
-			a.normal.clone().sub( this.normal ).multiplyScalar( t )
-		);
-
-		this.uv.add(
-			a.uv.clone().sub( this.uv ).multiplyScalar( t )
-		);
-
-		return this;
-	};
-	ThreeBSP.Vertex.prototype.interpolate = function( other, t ) {
-		return this.clone().lerp( other, t );
-	};
-	ThreeBSP.Vertex.prototype.applyMatrix4 = function ( m ) {
-
-		// input: THREE.Matrix4 affine matrix
-
-		var x = this.x, y = this.y, z = this.z;
-
-		var e = m.elements;
-
-		this.x = e[0] * x + e[4] * y + e[8]  * z + e[12];
-		this.y = e[1] * x + e[5] * y + e[9]  * z + e[13];
-		this.z = e[2] * x + e[6] * y + e[10] * z + e[14];
-
-		return this;
-
-	}
+	
 
 
 	ThreeBSP.Node = function( polygons ) {
