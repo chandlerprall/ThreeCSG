1b3518955a27de5ede50bfc440674d4b0e13c7f4
Wilt <wouter_wiltenburg@hotmail.com>
2014-09-21 17:29:34 +0200

    Bugfix: wrong index for vertexNormals for face.b
    There was a bug in converting vertex normals for `THREE.Face3` faces. The index value used to collect the vertexNormalsfor vertex b (`face.b`) was 2 instead of 1, resulting in a wrong geometry.

--- ThreeCSG.js
+++ ThreeCSG.js
@@ -45,7 +45,7 @@ window.ThreeBSP = (function() {
 				
 				vertex = geometry.vertices[ face.b ];
                                 uvs = faceVertexUvs ? new THREE.Vector2( faceVertexUvs[1].x, faceVertexUvs[1].y ) : null;
-                                vertex = new ThreeBSP.Vertex( vertex.x, vertex.y, vertex.z, face.vertexNormals[2], uvs );
+                                vertex = new ThreeBSP.Vertex( vertex.x, vertex.y, vertex.z, face.vertexNormals[1], uvs );
 				vertex.applyMatrix4(this.matrix);
 				polygon.vertices.push( vertex );
 				
