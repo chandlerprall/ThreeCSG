e5c64a6c29b1ca88a810874cac2634546de3d7fc
Alberto Valero <alberto.valero@bq.com>
2019-07-03 17:51:04 +0200

    no need to clone a,b,c as they will be cloned on triangle constructor

--- src/meshUtils.ts
+++ src/meshUtils.ts
@@ -1,33 +1,35 @@
-import {Geometry, Mesh} from 'three';
+import { Geometry, Mesh } from 'three';
 import Triangle from './Triangle';
-import {isBufferGeometry} from './utils';
+import { isBufferGeometry } from './utils';
 import BSPNode from './BSPNode';
 
-export function convertGeometryToTriangles(geometry: Mesh['geometry']): Triangle[] {
-    if (isBufferGeometry(geometry)) {
-        throw new Error('threecsg: Only Three.Geometry is supported.');
-    }
+export function convertGeometryToTriangles(
+  geometry: Mesh['geometry']
+): Triangle[] {
+  if (isBufferGeometry(geometry)) {
+    throw new Error('threecsg: Only Three.Geometry is supported.');
+  }
 
-    const triangles: Triangle[] = [];
+  const triangles: Triangle[] = [];
 
-    const { faces, vertices } = geometry;
-    for (let i = 0; i < faces.length; i++) {
-        const face = faces[i];
+  const { faces, vertices } = geometry;
+  for (let i = 0; i < faces.length; i++) {
+    const face = faces[i];
 
-        const a = vertices[face.a].clone();
-        const b = vertices[face.b].clone();
-        const c = vertices[face.c].clone();
+    const a = vertices[face.a];
+    const b = vertices[face.b];
+    const c = vertices[face.c];
 
-        const triangle = new Triangle(a, b, c);
+    const triangle = new Triangle(a, b, c);
 
-        triangles.push(triangle);
-    }
+    triangles.push(triangle);
+  }
 
-    return triangles;
+  return triangles;
 }
 
 export function transformBSP(bsp: BSPNode, mesh: Mesh) {
-    mesh.updateMatrixWorld(true);
-    const { matrixWorld: transform } = mesh;
-    return bsp.clone(transform);
+  mesh.updateMatrixWorld(true);
+  const { matrixWorld: transform } = mesh;
+  return bsp.clone(transform);
 }
