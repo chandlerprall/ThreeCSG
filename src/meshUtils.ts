1f823003f202de491c770af6d008248f6519322a
makc <makc.the.great@gmail.com>
2019-12-01 19:29:16 +0100

    import BufferGeometry

--- src/meshUtils.ts
+++ src/meshUtils.ts
@@ -1,4 +1,4 @@
-import { Geometry, Mesh } from 'three';
+import { Geometry, Mesh, Vector3 } from 'three';
 import Triangle from './Triangle';
 import { isBufferGeometry } from './utils';
 import BSPNode from './BSPNode';
@@ -6,11 +6,35 @@ import BSPNode from './BSPNode';
 export function convertGeometryToTriangles(
   geometry: Mesh['geometry']
 ): Triangle[] {
-  if (isBufferGeometry(geometry)) {
-    throw new Error('threecsg: Only Three.Geometry is supported.');
-  }
-
   const triangles: Triangle[] = [];
+
+  if (isBufferGeometry(geometry)) {
+    const position = geometry.getAttribute('position');
+    const index = geometry.getIndex();
+    if (index) {
+      for (let i = 0; i < index.array.length; i+=3) {
+        let j = index.array[i];
+        const a = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+        j = index.array[i + 1];
+        const b = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+        j = index.array[i + 2];
+        const c = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+
+        triangles.push(new Triangle(a, b, c));
+      }
+    } else {
+      for (let j = 0; j < position.count; j++) {
+        const a = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+        j++;
+        const b = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+        j++;
+        const c = new Vector3(position.getX(j), position.getY(j), position.getZ(j));
+
+        triangles.push(new Triangle(a, b, c));
+      }
+    }
+    return triangles;
+  }
 
   const { faces, vertices } = geometry;
   for (let i = 0; i < faces.length; i++) {
